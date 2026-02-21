fa84df63104fc32c58d13b063471963b1113e389
codeverse-amol <amolbs2109@gmail.com>
2026-02-21 19:53:27 +0530

    Base.py modified

--- config/settings/base.py
+++ config/settings/base.py
@@ -102,10 +102,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get(
-            "DATABASE_URL",
-            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-        )
+        default=os.getenv("DATABASE_URL")
     )
 }
 
