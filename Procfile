cba5aff85b4eb8b4792bf5266b0247f62fde37d0
codeverse-amol <amolbs2109@gmail.com>
2026-02-21 20:27:06 +0530

    updated changes

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn config.wsgi:application
-release: python manage.py migrate --settings=config.settings.production
+release: python manage.py migrate --settings=config.settings.production --noinput || true
--- config/settings/base.py
+++ config/settings/base.py
@@ -99,15 +99,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-
-
-
-
-DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600
-    )
-}
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600
+        )
+    }
+else:
+    # Fallback to SQLite for development
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
