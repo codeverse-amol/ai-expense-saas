513303b918784ae2def66a2963a1a84ae323e416
codeverse-amol <amolbs2109@gmail.com>
2026-02-21 20:03:25 +0530

    Fix DB config properly

--- config/settings/base.py
+++ config/settings/base.py
@@ -100,9 +100,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        conn_max_age=600
     )
 }
 
