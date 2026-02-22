52e19a344903b324155db216849155ed8c9850be
codeverse-amol <amolbs2109@gmail.com>
2026-02-22 15:26:29 +0530

    Register CategoryBudget and MonthlyBudget in Django admin

--- apps/expenses/admin.py
+++ apps/expenses/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from .models import Category, Expense
+from .models import Category, Expense, CategoryBudget, MonthlyBudget
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "user", "created_at")
+
+
+@admin.register(CategoryBudget)
+class CategoryBudgetAdmin(admin.ModelAdmin):
+    list_display = ("category", "user", "amount", "month", "year", "created_at")
+    list_filter = ("year", "month", "category")
+    search_fields = ("category__name", "user__email")
+
+
+@admin.register(MonthlyBudget)
+class MonthlyBudgetAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "month", "year", "created_at")
+    list_filter = ("year", "month")
+    search_fields = ("user__email",)
 
 
 @admin.register(Expense)
